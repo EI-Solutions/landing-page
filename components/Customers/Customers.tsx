ab3090b80578da6b9d0359a8cb12d41c458cb329
Carl Gützkow <cjgutzkow@gmail.com>
2022-12-16 17:44:38 +0100

    start a next project with tailwindcss and set up customer list

---
+++ components/Customers/Customers.tsx
@@ -0,0 +1,52 @@
+import Image from 'next/image'
+import ageraLogo from '/assets/customer-logos/agera-logo.png'
+import asplanViakLogo from '/assets/customer-logos/asplanviak-logo.png'
+import dnvLogo from '/assets/customer-logos/dnv-logo.png'
+import mollerEiendomLogo from '/assets/customer-logos/mollereiendom-logo.png'
+import baneNorLogo from '/assets/customer-logos/banenor-logo.png'
+
+
+const Customers = () => {
+    const customers = [
+        {
+            image: ageraLogo,
+            alt: 'Agera Eiendom'
+        },
+        {
+            image: asplanViakLogo,
+            alt: 'Asplan Viak'
+        },
+        {
+            image: dnvLogo,
+            alt: 'DNV'
+        },
+        {
+            image: mollerEiendomLogo,
+            alt: 'Møller Eiendom'
+        },
+        {
+            image: baneNorLogo,
+            alt: 'Bane Nor Eiendom'
+        },
+    ]
+
+    return (
+        <div className='ei-gradient py-20'>
+            <div className='align-middle grid grid-cols-3'>
+                {customers.map(customer => (
+                    <div
+                        key={customer.alt}
+                        className='h-40 flex justify-center'>
+
+                        <Image
+                            className='p-5 fit h-40'
+                            src={customer.image}
+                            alt={customer.alt}
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+export default Customers;
