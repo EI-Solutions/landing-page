62d7ead384eeb16079ce8f06970e0d327f9c48ce
Carl Gützkow <cjgutzkow@gmail.com>
2022-12-16 19:25:26 +0100

    add document header file and remove unused style file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Head from 'next/head';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <link href='https://fonts.googleapis.com/css?family=Poppins&display=optional' rel='stylesheet'/>
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
-
-export default MyApp
