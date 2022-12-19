dd3f64877d553fd1b74c7e6094802818549738e7
Carl Gützkow <cjgutzkow@gmail.com>
2022-12-19 13:54:30 +0100

    move navbar to landing component and use background image with overlay blend

--- components/MainLanding/MainLanding.tsx
+++ components/MainLanding/MainLanding.tsx
@@ -1,39 +1,38 @@
-import Image from "next/image"
-import houses from "/static/videos/houses-topdown.png"
+import NavigationBar from "../NavigationBar/NavigationBar"
+import eisolutionsLogo from '/static/eisolutions-logo.svg'
 
 
 const MainLanding = () => {
     return (
         <div
-            className="p-20"
+            className="bg-ei-dark-green/30"
         >
-            <div className="absolute top-0 left-0 -z-10 bg-ei-dark-green bg-cover">
-                <Image
-                    src={houses}
-                    alt="Houses"
-                    className="opacity-30"
-                />
-            </div>
-            <div className="w-1/2">
-                <p className="text-5xl m-5">
-                    Den første og mest automatiserte løsningen for analyse av eiendommer i henhold til EU-taksonomien
-                </p>
-                <p className="m-5">
-                    Vi benytter oss av romlig analyse av data for å generere automatisk dokumentasjon for å avgjøre om prosjekter er innenfor miljøkravene i EU-taksonomien
-                </p>
-                <div className="flex-row flex text-xl">
-                    <a
-                        href="https://app.eisolutions.no"
-                        className="bg-ei-dark-green p-4 m-5"
-                    >
-                        Til applikasjonen
-                    </a>
-                    <a 
-                        href="mailto:contact@eisolutions.no"
-                        className="bg-ei-green p-4 m-5"
-                    >
-                        Forespør demo
-                    </a>
+            <div 
+                style={{backgroundImage: 'url("/static/videos/houses-topdown.png")'}} 
+                className="bg-cover min-h-screen bg-ei-dark-green/70 bg-blend-darken"
+            >
+                <NavigationBar isTransparent={true} icon={eisolutionsLogo} />
+                <div className="w-1/2 p-20">
+                    <p className="text-5xl m-5">
+                        Den første og mest automatiserte løsningen for analyse av eiendommer i henhold til EU-taksonomien
+                    </p>
+                    <p className="m-5">
+                        Vi benytter oss av romlig analyse av data for å generere automatisk dokumentasjon for å avgjøre om prosjekter er innenfor miljøkravene i EU-taksonomien
+                    </p>
+                    <div className="flex-row flex text-xl">
+                        <a
+                            href="https://app.eisolutions.no"
+                            className="bg-ei-dark-green p-4 m-5"
+                        >
+                            Til applikasjonen
+                        </a>
+                        <a
+                            href="mailto:contact@eisolutions.no"
+                            className="bg-ei-green p-4 m-5"
+                        >
+                            Forespør demo
+                        </a>
+                    </div>
                 </div>
             </div>
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,12 @@ import type { NextPage } from 'next'
 import Customers from '../components/Customers/Customers'
 import SalePoints from '../components/SalesPoints/SalesPoints'
 import MainLanding from '../components/MainLanding/MainLanding'
-import NavigationBar from '../components/NavigationBar/NavigationBar'
 import Footer from '../components/Footer/Footer'
-import eisolutionsLogo from '/static/eisolutions-logo.svg'
 
 
 const Home: NextPage = () => {
   return (
     <>
-      <NavigationBar isTransparent={true} icon={eisolutionsLogo} />
       <MainLanding />
       <SalePoints />
       <Customers />
