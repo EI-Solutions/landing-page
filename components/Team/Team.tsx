dd3454f983c88686247adb1077f2977fc9fc7137
Carl Gützkow <cjgutzkow@gmail.com>
2022-12-22 15:06:51 +0100

    style text with color and position

--- components/Team/Team.tsx
+++ components/Team/Team.tsx
@@ -34,7 +34,7 @@ const Team = () => {
         },
         {
             image: alba,
-            name: 'Alba Arnesen',
+            name: 'Alba Marie Nesvik Arnesen',
             linkedIn: 'https://www.linkedin.com/in/alba-arnesen-3a179b254/',
             role: 'UX Designer',
         },
@@ -42,14 +42,16 @@ const Team = () => {
 
     return (
         <div className='p-20'>
-            <b className='text-4xl my-10 text-ei-dark-green'>Møt vårt
-                <b className='text-ei-green'> kreative</b> og
-                <b className='text-ei-green'> kompetente</b> team!
-            </b>
-            <p className='text-black'><i>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</i></p>
+            <div className='w-2/5'>
+                <span className='font-bold text-4xl text-ei-dark-green'>Møt vårt
+                    <span className='text-ei-green'> kreative</span> og
+                    <span className='text-ei-green'> kompetente</span> team!
+                </span>
+            </div>
+            <p className='text-ei-dark-green py-5 w-3/5'><i>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</i></p>
             <div className='align-middle grid lg:grid-cols-3 grid-cols-1'>
                 {team.map(member => (
-                    <div key={member.name} className='p-5 justify-center flex'>
+                    <div key={member.name} className='p-4 justify-center flex'>
                         <a href={member.linkedIn}>
                             <div className='w-min-content'>
                                 <Image
