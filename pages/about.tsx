fa0f91c4e32f6704bbb20e267a8f8846014c62f6
Carl Gützkow <cjgutzkow@gmail.com>
2022-12-27 14:43:52 +0100

    deleted unused images, change icon and added titles for both pages

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,10 +4,14 @@ import Footer from '../components/Footer/Footer'
 import NavigationBar from '../components/NavigationBar/NavigationBar'
 import Team from '../components/Team/Team'
 import eisolutionsLogo from '/static/eisolutions-logo-white.svg'
+import Head from 'next/head'
 
 const About: NextPage = () => {
   return (
     <>
+      <Head>
+        <title>Ei Solutions - Om oss</title>
+      </Head>
       <NavigationBar
         backgroundClasses='bg-transparent'
         textClasses='text-white'
