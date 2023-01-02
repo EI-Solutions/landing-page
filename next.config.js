2204fd977c94d85a2f01712eb23f70cbf014feed
Carl Gützkow <cjgutzkow@gmail.com>
2023-01-02 16:17:52 +0100

    create locale files for en and nb

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['en', 'nb'],
+    defaultLocale: 'en',
+  },
 }
 
 module.exports = nextConfig
