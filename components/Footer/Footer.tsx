c93a0056690cd148501cba8623524f4c35d5c91d
Carl Gützkow <cjgutzkow@gmail.com>
2023-01-03 12:06:39 +0100

    create a modal to store terms and cookies

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -4,9 +4,14 @@ import linkedin from '/static/icons/linkedin.svg';
 import ContactInformation from './ContactInformation';
 import Link from 'next/link';
 import { useTranslations } from 'next-intl';
+import Modal from '../Modal/Modal';
+import { useState } from 'react';
+
 
 const Footer = () => {
   const t = useTranslations('Footer');
+  const [showingTerms, setShowingTerms] = useState(false)
+  const [showingCookies, setShowingCookies] = useState(false)
 
   return (
     <footer
@@ -27,22 +32,18 @@ const Footer = () => {
           <a href="https://www.linkedin.com/company/ei-solutions-as/">
             <Image src={linkedin} alt="Linked In icon" className="m-3 w-10" />
           </a>
-          <div className="text-ei-green font-bold p-5">
-            <p className="my-2">
-              <Link href="/about">{t('about')}</Link>
-            </p>
-            <p className="my-2">
-              <Link href="/cookies">{t('cookies')}</Link>
-            </p>
-            <p className="my-2">
-              <Link href="/terms-and-conditions">{t('terms')}</Link>
-            </p>
+          <div className='text-ei-green font-bold p-5'>
+            <p className='my-2'><Link href="/about">{t('about')}</Link></p>
+            <p className='my-2'><button onClick={() => { setShowingCookies(true) }}>{t('cookies')}</button></p>
+            <p className='my-2'><button onClick={() => { setShowingTerms(true) }}>{t('terms')}</button></p>
           </div>
         </div>
       </div>
-      <p className="text-center p-5">
+      <p className='text-center p-5'>
         {t('rights1')} © 2022, Ei Solutions AS. {t('rights2')}
       </p>
+      <Modal title='Cookie Preferences' setShowing={setShowingCookies} modalCardClasses='translate-y-0 opacity-100' showing={showingCookies} />
+      <Modal title='Terms and Conditions' setShowing={setShowingTerms} modalCardClasses='translate-y-0 opacity-100' showing={showingTerms} />
     </footer>
   );
 };
