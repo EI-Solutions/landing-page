9f1e0f9abddd882d57b323c5d93f28b7c06c6b71
Carl Gützkow <cjgutzkow@gmail.com>
2023-01-25 22:36:38 +0100

    run prettier

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,34 +3,39 @@ import { useTranslations } from 'next-intl';
 import Link from 'next/link';
 import BasePage from '../components/BasePage/BasePage';
 
-
 const PageNotFound: NextPage = () => {
-    const t = useTranslations('Error.404');
-    return (
-        <BasePage subPageName='notFound'>
-            <div className='p-64 h-screen bg-gradient-to-r from-ei-black-green to-ei-dark-green'>
-                <p className=' text-[100px] font-bold'>404</p>
-                <p>{t("subtitle")}</p>
-                <p>{t.rich('linkIndex', {
-                    link: (text) => <Link href='/' className='underline'>{text}</Link>,
-                })}</p>
-            </div>
-        </BasePage>
-    )
-}
+  const t = useTranslations('Error.404');
+  return (
+    <BasePage subPageName="notFound">
+      <div className="p-64 h-screen bg-gradient-to-r from-ei-black-green to-ei-dark-green">
+        <p className=" text-[100px] font-bold">404</p>
+        <p>{t('subtitle')}</p>
+        <p>
+          {t.rich('linkIndex', {
+            link: (text) => (
+              <Link href="/" className="underline">
+                {text}
+              </Link>
+            ),
+          })}
+        </p>
+      </div>
+    </BasePage>
+  );
+};
 
 export default PageNotFound;
 
 interface Props {
-    locale: string;
+  locale: string;
 }
 
 export function getStaticProps({ locale }: Props) {
-    return {
-        props: {
-            messages: {
-                ...require(`/messages/${locale}.json`),
-            },
-        },
-    };
+  return {
+    props: {
+      messages: {
+        ...require(`/messages/${locale}.json`),
+      },
+    },
+  };
 }
